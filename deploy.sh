0ae2d45d18ef10da98cb1a11cea3cb1cf440ebc2
geezerP <geezerP@yahoo.com>
2020-07-09 10:48:42 +0300

    refactor gitlab-ci.yml file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /srv/mat-api
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $IMAGE:web
-  docker pull $IMAGE:nginx
+  docker pull $image:web
+  docker pull $image:nginx
   docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
