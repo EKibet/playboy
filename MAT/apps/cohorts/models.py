e976a64579ab893ffad68fcbb587fea6a5163654
Raphael Katana <roduor41@gmail.com>
2020-10-26 14:54:15 +0300

    refactor(cohort creation): limit cohort creation to admins or pod leaders - add permission class to cohort serializer - add start and end dates to cohort model - the end date is a timedelta of 15 weeks by default - add end date validator and remove is_staff from pod leader
    feat(refactor auth model)
    - Create proxy models for TM, Pod Leader & Student
    - Refactor tests to conform to new structure
    - Refactor auth Model to comply to SRP
    - Create TM profile model

--- MAT/apps/cohorts/models.py
+++ MAT/apps/cohorts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
-
 from MAT.apps.common.base import CommonFieldsMixin
-
-
+from datetime import date as d, timedelta as td
 class Cohort(CommonFieldsMixin):
     """
     This is the cohort model for both the students and tms.
     """
     name = models.CharField(max_length=25, unique=True)
+    start_date = models.DateField(default=d.today)
+    end_date = models.DateField(default=(d.today() + td(weeks=15)))
     # this is to track the changes on the model.
     history = HistoricalRecords()
 
--- MAT/tests/cohorts/test_cohort_crud.py
+++ MAT/tests/cohorts/test_cohort_crud.py
@@ -10,9 +10,9 @@ from MAT.apps.cohorts.models import Cohort
 
 class TestCohortCRUD():
     @pytest.mark.django_db
-    def test_get_cohorts_list_succeeds(self, client, get_or_create_token):
+    def test_get_cohorts_list_succeeds(self, client, get_or_create_podleader_token):
         url = reverse('cohorts:list')
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
         response = client.get(url)
         assert response.status_code == status.HTTP_200_OK
@@ -30,24 +30,25 @@ class TestCohortCRUD():
         assert str(saved_cohort) == "MC01"
 
     @pytest.mark.django_db
-    def test_create_cohort_succeeds(self, client, get_or_create_token):
+    def test_create_cohort_succeeds(self, client, get_or_create_podleader_token):
         """Test creating a cohort """
+        
         data = {
             "cohort_name": "MC22"
         }
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         url = reverse('cohorts:cohorts-list')
-
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
-        response = client.post(url, data=json.dumps(data),
-                               content_type='application/json')
+        response = client.post(url, data)
+        
         assert response.status_code == status.HTTP_201_CREATED
 
     @pytest.mark.django_db
-    def test_read_cohort_succeeds(self, client, get_or_create_token, new_cohort):
+    def test_read_cohort_succeeds(self, client, get_or_create_podleader_token, new_cohort):
         """Test getting an existing cohort """
+       
         new_cohort.save()
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         latest_cohort = Cohort.objects.latest('id')
         url = reverse('cohorts:cohorts-detail', args=[str(latest_cohort.id)])
 
@@ -56,28 +57,29 @@ class TestCohortCRUD():
         assert response.status_code == status.HTTP_200_OK
 
     @pytest.mark.django_db
-    def test_update_cohort_succeeds(self, client, get_or_create_token, new_cohort):
+    def test_update_cohort_succeeds(self, client, get_or_create_podleader_token, new_cohort):
         """Test editing of an existing cohort """
         new_cohort.save()
         update_data = {
             "cohort_name": "MC01"
         }
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         latest_cohort = Cohort.objects.latest('id')
         url = reverse('cohorts:cohorts-detail', args=[str(latest_cohort.id)])
 
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
         response = client.put(url, data=json.dumps(update_data),
-                              content_type='application/json')
+                            content_type='application/json')
         latest_cohort = Cohort.objects.latest('id')
         assert response.status_code == status.HTTP_200_OK
         assert latest_cohort.name == "MC01"
 
     @pytest.mark.django_db
-    def test_delete_cohort_succeeds(self, client, get_or_create_token, new_cohort):
+    def test_delete_cohort_succeeds(self, client, get_or_create_podleader_token, new_cohort):
         """Test deletion of an existing cohort """
+        
         new_cohort.save()
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         latest_cohort = Cohort.objects.latest('id')
         url = reverse('cohorts:cohorts-detail', args=[str(latest_cohort.id)])
 
@@ -86,12 +88,12 @@ class TestCohortCRUD():
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
     @pytest.mark.django_db
-    def test_get_tm_cohorts_list_succeeds(self, client, get_or_create_token):
+    def test_get_tm_cohorts_list_succeeds(self, client, get_or_create_podleader_token):
         url = reverse('cohorts:tm-list')
-        token = get_or_create_token
+        token = get_or_create_podleader_token
         cohort1 = Cohort.objects.create(name="MC32")
         cohort2 = Cohort.objects.create(name="MC33")
-        current_usr = User.objects.get(email='test@mail.com')
+        current_usr = User.objects.get(email='podleader@mail.com')
         current_usr.cohort.add(cohort1,cohort2)
         
         client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
